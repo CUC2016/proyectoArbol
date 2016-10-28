76a64b54bbca81446e0fa76c72d1e36a84a4fcd0
Daniel Vergara <danielvsuarez01@gmail.com>
2016-10-28 10:40:40 -0500

    Se modifico el menu de usuario

--- index.php
+++ index.php
@@ -33,71 +33,79 @@
          </ul>
        </div>
     </nav>
+
     <div class="container">
-      <ul class="collapsible" data-collapsible="accordion">
-        <li>
-          <div class="collapsible-header"><i class="material-icons">filter_drama</i>Primeros pasos</div>
-          <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p>
-            <!--crear Arbol-->
-            <div class="row">
-              <div class="col s6 m4 l3">
-                <div class="card-panel">
-                  <form class="center-align" action="index2.php" method="post">
+      <a href="#" data-activates="nav-mobile" class="button-collapse top-nav full hide-on-large-only">
+        <i class="material-icons">menu</i></a>
+    </div>
+    <ul id="nav-mobile" class="side-nav fixed">
+        <li class="no-padding">
+          <ul class="collapsible collapsible-accordion">
+            <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Primeros Pasos</a>
+              <div class="collapsible-body">
+                <ul>
+                  <!--aqui van los metodos que agregan y tal-->
+                  <!--crear Arbol-->
+                  <div class="row">
                     <div class="">
-                      Crear Arbol
-                      <input placeholder="Nombre de la Raiz" type="text" name="nodoRaiz" class="validate">
-                      <button class="btn waves-effect waves-light " type="submit" name="action">Crear Arbol
-                        <i class="material-icons right">send</i>
-                      </button>
+                      <div class="card-panel">
+                        <form class="center-align" action="index2.php" method="post">
+                          <div class="">
+                            Crear Arbol
+                            <input placeholder="Nombre de la Raiz" type="text" name="nodoRaiz" class="validate">
+                            <button class="btn waves-effect waves-light " type="submit" name="action">Crear Arbol
+                              <i class="material-icons right">send</i>
+                            </button>
+                          </div>
+                        </form>
+                      </div>
                     </div>
-                  </form>
-                </div>
-              </div>
 
-              <div class="col s6 m4 l3">
-                <div class="card-panel">
-                  <form class="center-align" action="index2.php" method="post">
                     <div class="">
-                      Crear Nodos
-                      <input placeholder="Nombre del Papa" type="text" name="nodoPadre" class="validate">
-                      <input placeholder="Nombre del Hijo" type="text" name="nodoHijo" class="validate">
-                      <input type="radio" name="group1" id="test1">
-                      <label for="test1">Derecha</label>
-                      <input type="radio" name="group1" id="test2">
-                      <label for="test2">Izquierda</label>
-                      <button class="btn waves-effect waves-light " type="submit" name="action">Agregar Nodos
-                        <i class="material-icons right">send</i>
-                      </button>
+                      <div class="card-panel">
+                        <form class="center-align" action="index2.php" method="post">
+                          <div class="">
+                            Crear Nodos
+                            <input placeholder="Nombre del Papa" type="text" name="nodoPadre" class="validate">
+                            <input placeholder="Nombre del Hijo" type="text" name="nodoHijo" class="validate">
+                            <input type="radio" name="group1" id="test1">
+                            <label for="test1">Derecha</label>
+                            <input type="radio" name="group1" id="test2">
+                            <label for="test2">Izquierda</label>
+                            <button class="btn waves-effect waves-light " type="submit" name="action">Agregar Nodos
+                              <i class="material-icons right">send</i>
+                            </button>
+                          </div>
+                        </form>
+                      </div>
                     </div>
-                  </form>
-                </div>
-              </div>
 
-              <div class="col s6 m4 l3">
-                <div class="card-panel">
-                  <form class="center-align" action="index2.php" method="post">
                     <div class="">
-                      Eliminar Nodo
-                      <input placeholder="Nombre del Nodo" type="text" name="eliminarNodo" class="validate">
-                      <button class="btn waves-effect waves-light " type="submit" name="action">Eliminar Nodo
-                        <i class="material-icons right">send</i>
-                      </button>
+                      <div class="card-panel">
+                        <form class="center-align" action="index2.php" method="post">
+                          <div class="">
+                            Eliminar Nodo
+                            <input placeholder="Nombre del Nodo" type="text" name="eliminarNodo" class="validate">
+                            <button class="btn waves-effect waves-light " type="submit" name="action">Eliminar Nodo
+                              <i class="material-icons right">send</i>
+                            </button>
+                          </div>
+                        </form>
+                      </div>
                     </div>
-                  </form>
-                </div>
+                </ul>
               </div>
-
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="collapsible-header"><i class="material-icons">place</i>Procesos</div>
-          <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+            </li>
+            <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Procesos</a>
+              <div class="collapsible-body">
+                <ul>
+                  <!--Aqui van los componentes-->
+                </ul>
+              </div>
+            </li>
+          </ul>
         </li>
       </ul>
-    </div>
-
-
     <!--Importar archivos js-->
      <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
      <script type="text/javascript" src="js/materialize.min.js"></script>
